afdbe6a0619dbcd2fb85810fc9ab758339362bd7
dinigo <dinigo@movisat.com>
2019-12-29 20:55:55 +0100

    Primer commit

---
+++ Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class CambiarEscena : MonoBehaviour, IPointerClickHandler {
+
+    public Escenas cargarEscena;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PantallaDeCarga.Instancia.CargarEscena(cargarEscena.ToString());
+    }
+
+}
+
+public enum Escenas
+{
+    Escena1,
+    Escena2
+}
---
+++ Assets/Scripts/PantallaDeCarga.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PantallaDeCarga : MonoBehaviour {
+    
+    public static PantallaDeCarga Instancia { get; private set; }
+
+    public Image imageDeCarga;
+    [Range(0.01f, 0.1f)]
+    public float velocidadAparecer = 0.5f;
+    [Range(0.01f, 0.1f)]
+    public float velocidadOcultar = 0.5f;
+
+    void Awake()
+    {
+        DefinirSingleton();
+    }
+
+    private void DefinirSingleton()
+    {
+        if (Instancia == null)
+        {
+            Instancia = this;
+            DontDestroyOnLoad(this);
+            imageDeCarga.gameObject.SetActive(false);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CargarEscena(string nombreEscena)
+    {
+        StartCoroutine(MostrarPantallaDeCarga(nombreEscena));
+    }
+    
+
+    private IEnumerator MostrarPantallaDeCarga(string nombreEscena)
+    {
+        imageDeCarga.gameObject.SetActive(true);
+        Color c = imageDeCarga.color;
+        c.a = 0.0f;
+
+        //Mientras no esté totalmente visible va aumentando su visibilidad
+        while(c.a < 1)
+        {
+            imageDeCarga.color = c;
+            c.a += velocidadAparecer;
+            yield return null;
+        }
+
+        //Carga la escena
+        SceneManager.LoadScene(nombreEscena);
+
+        //Espera a que haya cargado la nueva escena
+        while (!nombreEscena.Equals(SceneManager.GetActiveScene().name))
+        {
+            yield return null;
+        }
+
+        //Mientras la imagen de carga siga visible va desvaneciéndola
+        while (c.a > 0)
+        {
+            imageDeCarga.color = c;
+            c.a -= velocidadOcultar;
+            yield return null;
+        }
+        imageDeCarga.gameObject.SetActive(false);
+    }
+}
